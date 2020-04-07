a80be6680f373cb10b0fdd306312b1a471ef65bd
Luvpreet Singh <luvpreet.singh@vogo.in>
2020-04-07 20:42:33 +0530

    update(settings): accept debug flag from env vars and update sample_vars

--- calendlo/calendlo/settings.py
+++ calendlo/calendlo/settings.py
@@ -3,12 +3,16 @@ from .base_settings import * # noqa
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("CALENDLO_DATABASE_NAME", "NAME"),
-        'USER': os.getenv("CALENDLO_DATABASE_USER", "USER"),
-        'PASSWORD': os.getenv("CALENDLO_DATABASE_PASSWORD", "PASSWORD"),
-        'HOST': os.getenv("CALENDLO_DATABASE_HOST", "HOST"),
+        'NAME': os.getenv("CALENDLO_DATABASE_NAME"),
+        'USER': os.getenv("CALENDLO_DATABASE_USER"),
+        'PASSWORD': os.getenv("CALENDLO_DATABASE_PASSWORD"),
+        'HOST': os.getenv("CALENDLO_DATABASE_HOST", "localhost"),
         'PORT': int(os.getenv("CALENDLO_DATABASE_PORT", "5432")),
     }
 }
 
 NO_OF_WEEKS_TO_SCHEDULE = int(os.getenv("CALENDLO_WEEKS_TO_SCHEDULE", 4))
+
+SECRET_KEY = os.getenv("CALENDLO_SECRET_KEY")
+
+DEBUG = bool(os.getenv("CALENDLO_DEBUG", "0"))
