f08f8b03f4911790b358ca961e3ac1fad70304f8
Luvpreet Singh <luvpreet.singh@vogo.in>
2020-04-07 19:07:28 +0530

    tests(availability): add unit tests for AvailabilitySlot model

--- calendlo/availability/tests.py
+++ calendlo/availability/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+# python level imports
+import datetime
 
-# Create your tests here.
+# django level imports
+from django.test import TestCase
+from django.utils import timezone
+
+# project level imports
+from .models import AvailabilitySlot
+from accounts.models import CalendloUser
+from appointments.models import Appointment
+from libs.constants import DAYS_IN_WEEK
+
+
+class AvailabilitySlotTestCase(TestCase):
+
+    def setUp(self):
+        self.identifier = "tom_the_catto"
+        self.email = "tom_the_cat@gmail.in"
+        self.user = CalendloUser.objects.create(
+            identifier=self.identifier,
+            email=self.email
+        )
+        self.date_1 = timezone.now().date() + datetime.timedelta(3)
+        self.date_2 = timezone.now().date() + datetime.timedelta(4)
+        self.start_time = datetime.time(hour=9, minute=0, second=0)
+        self.slot_1 = AvailabilitySlot.objects.create(
+            date=self.date_1,
+            start_time=self.start_time,
+            user=self.user
+        )
+        self.slot_2 = AvailabilitySlot.objects.create(
+            date=self.date_2,
+            start_time=self.start_time,
+            user=self.user
+        )
+        self.appointment = Appointment.objects.create(
+            slot=self.slot_1,
+            title="Meeting with Jerry",
+            appointee_email="jerry@gmail.com",
+            appointee_name="Jerry",
+            description="Resolve Conflicts"
+        )
+
+    def test_slot(self):
+        self.assertTrue(self.slot_1.is_booked is True)
+        self.assertTrue(self.slot_2.is_booked is False)
+        self.assertTrue(self.slot_1, "{} on {} at {}".format(
+            self.identifier,
+            self.date_1,
+            self.start_time
+            )
+        )
+        self.assertTrue(isinstance(self.slot_1.get_appointment(), Appointment))
+        self.assertTrue(self.slot_2.get_appointment() is None)
+        self.assertTrue(self.slot_1.day in DAYS_IN_WEEK.keys())
+        self.assertTrue(isinstance(self.slot_1.delete_appointment(), Appointment))
+        self.assertTrue(self.slot_2.delete_appointment() is None)
+
+        with self.assertRaises(Appointment.DoesNotExist):
+            # appointment has been deleted in the above test
+            Appointment.objects.get(slot=self.slot_1)
